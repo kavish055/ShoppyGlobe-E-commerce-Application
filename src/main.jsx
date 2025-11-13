cf8156d6725f275eec3946fd76eaa288721cd099
kavish055 <144641647+kavish055@users.noreply.github.com>
2025-11-13 22:27:54 +0530

    Wrap application in StrictMode component

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { StoreProvider } from './context/store.jsx'
-import { Router } from './context/router.jsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { StoreProvider } from './context/store.jsx';
+import { Router } from './context/router.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StoreProvider>
-  <Router>
-    <App />
-  </Router>
-</StoreProvider>
-,
-)
+  <StrictMode>
+    <StoreProvider>
+      <Router>
+        <App />
+      </Router>
+    </StoreProvider>
+  </StrictMode>
+);
